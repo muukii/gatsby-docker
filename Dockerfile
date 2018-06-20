d26f3147b68b9e1c31e5da61700d6c3b14062cd7
Muukii <muukii.app@gmail.com>
2018-06-20 18:11:16 +0900

    Use yarn

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,8 @@
-FROM node:9.5-alpine
+FROM node:10.4.1
 
 EXPOSE 8000
 
-RUN apk update && \
-    apk add --update --repository http://dl-3.alpinelinux.org/alpine/edge/testing vips-tools vips-dev fftw-dev gcc g++ make libc6-compat && \
-    apk add git && \
-    apk add python && \
-    rm -rf /var/cache/apk/*
-
-RUN npm install --global gatsby --no-optional gatsby@1.9 
+RUN npm install --global gatsby --no-optional gatsby@1.9
 
 RUN mkdir -p /site
 WORKDIR /site
--- entry.sh
+++ entry.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 export GATSBY_DIR="/site"
@@ -13,31 +13,32 @@ then
 else
   if [ ! -e "$GATSBY_DIR/node_modules/" ]
   then
-    echo "Node modules is empty. Running npm install..."
-    npm install
+    echo "Node modules is empty. Running yarn install..."
+    yarn
 
   fi
 
 fi
 
 # Decide what to do
-if  [ "$1" == "develop" ]
-then
+if [[ "$1" == "develop" ]]; then
+
   rm -rf $GATSBY_DIR/public
   gatsby develop --host 0.0.0.0
 
-elif  [ "$1" == "build" ]
-then
+elif [[ "$1" == "build" ]]; then
+
   rm -rf $GATSBY_DIR/public
   gatsby build
 
-elif  [ "$1" == "stage" ]
-then
+elif [[ "$1" == "stage" ]]; then
+
   rm -rf $GATSBY_DIR/public
   gatsby build
   gatsby serve --port 8000
 
 else
+
   exec $@
 
 fi
